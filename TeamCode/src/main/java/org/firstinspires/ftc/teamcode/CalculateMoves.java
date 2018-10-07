39c855bb9feb30d41a4f664d1b9c8ea74755663c
Noa <noasouccar@icloud.com>
2018-10-07 12:55:08 -0400

    added imu test file

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CalculateMoves.java
@@ -0,0 +1,25 @@
+package org.firstinspires.ftc.teamcode;
+
+public class CalculateMoves {
+
+    private Float currXVal;
+    private Float currYVal;
+    private Float futXVal;
+    private Float futYVal;
+
+
+
+    public CalculateMoves(Float currX, Float currY, Float futX, Float futY) {
+        currXVal = currX;
+        currYVal = currY;
+        futXVal = futX;
+        futYVal = futY;
+    }
+
+   private static void getInchesToMove() {
+
+
+
+
+   }
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/IMUTest.java
@@ -0,0 +1,142 @@
+/* Copyright (c) 2017 FIRST. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted (subject to the limitations in the disclaimer below) provided that
+ * the following conditions are met:
+ *
+ * Redistributions of source code must retain the above copyright notice, this list
+ * of conditions and the following disclaimer.
+ *
+ * Redistributions in binary form must reproduce the above copyright notice, this
+ * list of conditions and the following disclaimer in the documentation and/or
+ * other materials provided with the distribution.
+ *
+ * Neither the name of FIRST nor the names of its contributors may be used to endorse or
+ * promote products derived from this software without specific prior written permission.
+ *
+ * NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE GRANTED BY THIS
+ * LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.hardware.bosch.BNO055IMU;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DigitalChannel;
+
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
+import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
+import org.firstinspires.ftc.robotcore.external.navigation.Position;
+import org.firstinspires.ftc.robotcore.external.navigation.Velocity;
+
+
+import com.qualcomm.hardware.bosch.BNO055IMU;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DigitalChannel;
+
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
+import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
+import org.firstinspires.ftc.robotcore.external.navigation.Position;
+import org.firstinspires.ftc.robotcore.external.navigation.Velocity;
+
+@Autonomous(name="Drive Avoid Imu", group="Exercises")
+//@Disabled
+public class IMUTest extends LinearOpMode
+{
+    DcMotor                 leftMotor;
+    DcMotor                 rightMotor;
+    DigitalChannel          touch;
+    BNO055IMU               imu;
+    Double lastY;
+    Double lastX;
+    Double lastZ;
+    Orientation             lastAngles = new Orientation();
+    double globalAngle, power = .30, correction;
+    boolean                 aButton, bButton, touched;
+
+    // called when init button is  pressed.
+    @Override
+    public void runOpMode()
+    {
+        //eftMotor = hardwareMap.dcMotor.get("left_motor");
+
+       // rightMotor = hardwareMap.dcMotor.get("right_motor");
+        //rightMotor.setDirection(DcMotor.Direction.REVERSE);
+
+        //leftMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        //rightMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+        // get a reference to REV Touch sensor.
+        //touch = hardwareMap.digitalChannel.get("touch_sensor");
+
+        BNO055IMU.Parameters parameters = new BNO055IMU.Parameters();
+
+        parameters.mode                = BNO055IMU.SensorMode.IMU;
+        parameters.angleUnit           = BNO055IMU.AngleUnit.DEGREES;
+        parameters.accelUnit           = BNO055IMU.AccelUnit.METERS_PERSEC_PERSEC;
+        parameters.loggingEnabled      = false;
+
+        // Retrieve and initialize the IMU. We expect the IMU to be attached to an I2C port
+        // on a Core Device Interface Module, configured to be a sensor of type "AdaFruit IMU",
+        // and named "imu".
+        imu = hardwareMap.get(BNO055IMU.class, "imu");
+
+        imu.initialize(parameters);
+
+        telemetry.addData("Mode", "calibrating...");
+        telemetry.update();
+
+        // make sure the imu gyro is calibrated before continuing.
+
+
+        // wait for start button.
+
+        waitForStart();
+
+        telemetry.addData("Mode", "running");
+        telemetry.update();
+
+        sleep(1000);
+
+        // drive until end of period.
+
+
+        while (opModeIsActive() && !imu.isGyroCalibrated())
+        {
+            // Use gyro to drive in a straight line.
+
+
+            lastX = imu.getPosition().x;
+            lastY = imu.getPosition().y;
+            lastZ = imu.getPosition().z;
+
+
+
+
+            telemetry.addData("1 imu x", imu.getAcceleration());
+            telemetry.addData("2 imu y", lastY);
+            telemetry.addData("3 imu z", lastZ);
+            telemetry.addData("angle", imu.getAngularOrientation());
+            telemetry.update();
+
+
+        }
+    }
+}
