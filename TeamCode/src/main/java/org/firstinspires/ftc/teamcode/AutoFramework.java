eb4cef71e2d5f3082c829ec141aa2f22dddce5df
RisBan <43934791+RisBan@users.noreply.github.com>
2018-12-02 14:14:12 -0500

    Basic AutoFramework - nothing in it yet.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoFramework.java
@@ -0,0 +1,15 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+
+@Autonomous(name = "Auto Framework", group = "Autonomous")
+public class AutoFramework {
+    enum AutoType {AutoRed, AutoBlue}
+    AutoType autoType;
+
+    //Hardware robot = new Hardware();
+
+
+
+
+}
