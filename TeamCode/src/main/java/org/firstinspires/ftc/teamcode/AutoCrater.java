cc1ed1efce9790409215f2925cf46a7e0dea5c0e
RisBan <43934791+RisBan@users.noreply.github.com>
2018-12-12 19:06:16 -0500

    First Autonomous - has lift lowering method.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoCrater.java
@@ -0,0 +1,8 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+
+@Autonomous(name="Auto Crater", group = "Autonomous")
+public class AutoCrater extends AutoBase {
+    public AutoCrater(){};
+}
